d17127b0bf5ec5121830023ac89c0d6ff8c79588
Vitalii Sindiakov <vitaliysindjakov@gmail.com>
2024-11-19 16:30:19 +0200

    homework lesson 18

---
+++ lesson_18/lesson_18_decorator.py
@@ -0,0 +1,35 @@
+# Напишіть декоратор, який логує аргументи та результати викликаної функції.
+# Створіть декоратор, який перехоплює та обробляє винятки, які виникають в ході виконання функції.
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    handlers=[logging.StreamHandler()])
+logger = logging.getLogger("logs")
+
+
+def log_arguments_and_result(func):
+    def wrapper(*args, **kwargs):
+        logging.info(f"Function: {func.__name__} with args: {args}, kwargs: {kwargs}")
+        result = None
+        try:
+            result = func(*args, **kwargs)
+            logging.info(f"Function: {func.__name__} returned: {result}")
+            return func(*args, **kwargs)
+        except Exception as e:
+            logging.error(f"Exception occurred in function {func.__name__}: {e}")
+        return result
+
+    return wrapper
+
+
+@log_arguments_and_result
+def find_el_in_list(elements: list, index: int):
+    if index < len(elements):
+        return elements[index]
+    else:
+        raise ValueError
+
+
+find_el_in_list(["Alex", "Den", "Ivan"], 1)
+find_el_in_list(["Alex", "Den", "Ivan"], 4)
