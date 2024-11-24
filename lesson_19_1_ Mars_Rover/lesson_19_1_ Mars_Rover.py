47af21a094120ded97e1d003114e387ec85d78de
Vitalii Sindiakov <vitaliysindjakov@gmail.com>
2024-11-24 16:47:02 +0200

    moved to Pytests

--- lesson_19_1_ Mars_Rover/lesson_19_1_ Mars_Rover.py
+++ lesson_19_1_ Mars_Rover/lesson_19_1_ Mars_Rover.py
@@ -1,10 +1,14 @@
 import requests
+import pytest
 
-url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos'
-params = {'sol': 1000, 'camera': 'fhaz', 'api_key': 'DEMO_KEY'}
-response = requests.get(url, params=params)
-
-if response.status_code == 200:
+@pytest.mark.swapi
+@pytest.mark.parametrize("url, params", [
+    ('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos',
+     {'sol': 1000, 'camera': 'fhaz', 'api_key': 'DEMO_KEY'})
+])
+def test_download_photo(url, params):
+    response = requests.get(url, params=params)
+    assert response.status_code == 200
     data = response.json()
     photos = data.get('photos', [])
     counter = 1
@@ -13,5 +17,5 @@ if response.status_code == 200:
         with open("mars_photo" + str(counter) + ".jpg", "wb") as file:
             counter += 1
             img_response = requests.get(img_url)
-            if img_response.status_code == 200:
-                file.write(img_response.content)
+            assert img_response.status_code == 200
+            file.write(img_response.content)
