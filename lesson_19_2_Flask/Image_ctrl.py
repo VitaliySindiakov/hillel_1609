7825a142d664f5156c6586abccca73c1685fbcc5
Vitalii Sindiakov <vitaliysindjakov@gmail.com>
2024-11-24 21:26:13 +0200

    homework lesson_19_2

---
+++ lesson_19_2_Flask/Image_ctrl.py
@@ -0,0 +1,25 @@
+import requests
+
+
+class ImageCtrl:
+    def __init__(self, url="http://127.0.0.1:8080"):
+        self.url = url
+
+    def upload_image(self, image_path):
+        with open(image_path, "rb") as image_file:
+            data = {"image": image_file.read()}
+            response = requests.post(f"{self.url}/upload", files=data)
+            if response.status_code != 201:
+                print("Image isn't downloaded")
+            return response.json()["image_url"]
+
+    def get_image_url(self, filename, headers=None):
+        response = requests.get(f"{self.url}/image/{filename}", headers=headers)
+        return response.json()["image_url"]
+
+    def delete_image(self, filename):
+        response = requests.delete(f"{self.url}/delete/{filename}")
+        return response.json()
+
+    if __name__ == "__main__":
+        pass
---
+++ lesson_19_2_Flask/lesson_19_2.py
@@ -0,0 +1,24 @@
+import os
+import pytest
+from Image_ctrl import ImageCtrl
+
+current_dir = os.path.dirname(__file__)
+
+def test_upload():
+    file_path = os.path.join(current_dir, "images", "example.jpg")
+    image_url: str = ImageCtrl().upload_image(file_path)
+    assert "image" in image_url
+
+
+def test_get_image_url():
+    headers = {'Content-Type': "text"}
+    image_url: str = ImageCtrl().get_image_url("image", headers)
+    assert "image" in image_url
+
+
+def test_delete_image():
+    response = ImageCtrl().delete_image("image")
+    assert response == 200
+
+
+
