164853287ecbc1ed933c18746f02145855f14313
Vitalii Sindiakov <vitaliysindjakov@gmail.com>
2024-12-16 13:56:06 +0200

    lesson_22 homework

--- lesson_22/lesson_22.py
+++ lesson_22/lesson_22.py
@@ -1,15 +1,22 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Table
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 import random
 
 Base = declarative_base()
+association_table = Table(
+    'association_table', Base.metadata,
+    Column('student_id', Integer, ForeignKey('students.id'), primary_key=True),
+    Column('course_id', Integer, ForeignKey('courses.id'), primary_key=True)
+)
+
 
 class Course(Base):
     __tablename__ = 'courses'
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
-    students = relationship("Student", back_populates="course")
+
+    students = relationship("Student", secondary=association_table, back_populates="courses")
 
 
 class Student(Base):
@@ -19,15 +26,13 @@ class Student(Base):
     name = Column(String, nullable=False)
     age = Column(Integer)
 
-    course_id = Column(Integer, ForeignKey("courses.id"))
-    course = relationship("Course", back_populates="students")
+    courses = relationship("Course", secondary=association_table, back_populates="students")
+
 
 engine = create_engine("sqlite:///example.db", echo=True)
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
-
-
 
 all_courses = [
     Course(name="History"),
@@ -36,25 +41,25 @@ all_courses = [
     Course(name="Mathematics")
 ]
 all_students = [
-    Student(name='John', age=30, course=random.choice(all_courses)),
-    Student(name='Vitalii', age=35, course=random.choice(all_courses)),
-    Student(name='Andrew', age=33, course=random.choice(all_courses))
+    Student(name='John', age=30, courses=random.sample(all_courses, 1)),
+    Student(name='Vitalii', age=35, courses=random.sample(all_courses, 2)),
+    Student(name='Andrew', age=33, courses=random.sample(all_courses, 3))
 ]
 
 
 def print_all_students():
-    print("print all students:")
+    print("Print all students:")
     for student in session.query(Student).all():
-        print(f"Student: {student.name}, Age: {student.age}, Course: {student.course.name}")
+        course_names = ", ".join(course.name for course in student.courses)
+        print(f"Student: {student.name}, Age: {student.age}, Courses: {course_names}")
 
 
-def add_new_student(name, age, course):
-    course_db = session.query(Course).filter_by(name=course.name).first()
-    student = Student(name=name, age=age, course=course)
-    print(f"Adding new Student: {student.name}, Age: {student.age}, Course: {student.course.name}")
-    if course_db:
-        session.add(student)
-        session.commit()
+def add_new_student(name, age, courses):
+    course_names = ", ".join(course.name for course in courses)
+    student = Student(name=name, age=age, courses=courses)
+    print(f"Adding new Student: {student.name}, Age: {student.age}, Courses: {course_names}")
+    session.add(student)
+    session.commit()
 
 
 def delete_student(name):
@@ -69,13 +74,13 @@ def get_student_by_name(name):
     return session.query(Student).filter_by(name=name).first()
 
 
-def update_student(name, age, course):
+def update_student(name, age, courses):
     student = session.query(Student).filter_by(name=name).first()
     print("updating student")
     if student:
         print("Student = ", student)
         student.age = age
-        student.course = course
+        student.courses = courses
         session.commit()
 
 
@@ -85,11 +90,12 @@ try:
     session.commit()
     session.add_all(all_students)
     session.commit()
-    add_new_student("William", 45, course=random.choice(all_courses))
-    update_student("John", 31, course=random.choice(all_courses))
+    add_new_student("William", 45, courses=random.sample(all_courses, 3))
+    update_student("John", 31, courses=random.sample(all_courses, 3))
     delete_student("Andrew")
     print_all_students()
-except:
+except Exception as e:
     session.rollback()
+    print(f" Exception occurred: {e}")
 finally:
     session.close()
